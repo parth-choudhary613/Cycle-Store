d1116601efa4eb2a2f5e13bdb72f62e474fc0774
Parth Choudhary <parthchoudhary4372@gmail.com>
2024-10-25 22:58:28 +0530

    practice

---
+++ src/practice.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function practice() {
+  return (
+    <>
+    <div>
+        <form>
+            <input type='text'> enter your name
+            </input>
+        </form>
+    </div>
+    </>
+  )
+}
+
+export default practice
